3880a685d12b2613f802899e21a091000cd35d27
BPun <Bel.Pun@corfinancialgroup.com>
2015-03-19 18:11:29 +0000

    added the job

---
+++ src/main/java/com/nepse/job/JobLauncherApp.java
@@ -0,0 +1,43 @@
+package com.nepse.job;
+
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.JobParameters;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.nepse.config.DefaultDatabaseConfig;
+import com.nepse.report.JobConfig;
+
+public class JobLauncherApp {
+
+	public static void main(String[] args) {
+		String jobName = args[0];
+		
+		ApplicationContext context = new AnnotationConfigApplicationContext(DefaultDatabaseConfig.class, JobConfig.class);
+		 
+		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
+		Job job = (Job) context.getBean(jobName);
+	 
+		try {
+	 
+			JobExecution execution = jobLauncher.run(job, new JobParameters());
+			System.out.println("Exit Status : " + execution.getStatus());
+			if(execution.getStatus().equals("FAILED")) {
+				
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+//	 
+		System.out.println("Done");
+		((ConfigurableApplicationContext)context).close();
+	 
+	
+
+	}
+
+}
