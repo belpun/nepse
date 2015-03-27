952f60050bc214d76eeb201beb8823f1df8d50c8
BPun <Bel.Pun@corfinancialgroup.com>
2015-03-27 17:43:51 +0000

    added ema

--- src/main/java/com/nepse/analysis/TechnicalAnalysisService.java
+++ src/main/java/com/nepse/analysis/TechnicalAnalysisService.java
@@ -1,9 +1,5 @@
 package com.nepse.analysis;
 
-import java.lang.reflect.InvocationTargetException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -16,12 +12,13 @@ import com.nepse.exception.InsufficientDataException;
 import com.tictactec.ta.lib.Core;
 import com.tictactec.ta.lib.MInteger;
 import com.tictactec.ta.lib.RetCode;
-import com.tictactec.ta.lib.meta.helpers.SimpleHelper;
 
 @Service
 public class TechnicalAnalysisService {
 
 	private final JDBCCompanyRepository companyRepository;
+
+	Core c = new Core();
 
 	@Autowired
 	public TechnicalAnalysisService(JDBCCompanyRepository companyRepository) {
@@ -63,81 +60,58 @@ public class TechnicalAnalysisService {
 		MInteger lOutIdx = new MInteger();
 		MInteger lOutSize = new MInteger();
 
-		String func = "rsi";
-		List<String> params = new ArrayList<String>();
-
-		params.clear();
-		params.add("14");
-
-		SimpleHelper calc = new SimpleHelper(func, params);
-
 		System.out.println("===============================================");
-		System.out.println(func);
-		try {
-			calc.calculate(0, close.length - 1, new Object[] { close },
-					new Object[] { output1 }, lOutIdx, lOutSize);
-			System.out.println("lookback=" + calc.getLookback());
-			System.out.println("outBegIdx    = " + lOutIdx.value
-					+ "    outNbElement = " + lOutSize.value);
+		RetCode retCode = c.rsi(0, close.length - 1, close, 14, lOutIdx,
+				lOutSize, output1);
 
-			for (int i = 0; i < output1.length; i++) {
-				System.out.println("output1[" + i + "]=" + output1[i]);
-			}
-
+		if (retCode == RetCode.Success) {
 			return output1;
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (NoSuchMethodException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
+		} else {
+			System.out.println("Error");
+			return null;
 		}
-
-		return null;
 	}
-	
-//	public static void main(String[] args) {
-//		double[] closePrice = {8938,
-//				6892,
-//				9433,
-//				10692,
-//				9713,
-//				8740,
-//				14300,
-//				15092,
-//				12276,
-//				9405,
-//				10813,
-//				11055
-//		};
-		
-//		Map<Long, Double> closingPrice = new HashMap<Long, Double>();
-//		
-//		closingPrice.put(new Long(1l), new Double(8938));
-//		closingPrice.put(new Long(2l), new Double(6892));
-//		closingPrice.put(new Long(3l), new Double(9433));
-//		closingPrice.put(new Long(4l), new Double(10692));
-//		closingPrice.put(new Long(5l), new Double(9713));
-//		closingPrice.put(new Long(6l), new Double(8740));
-//		closingPrice.put(new Long(7l), new Double(14300));
-//		closingPrice.put(new Long(8l), new Double(15092));
-//		closingPrice.put(new Long(9l), new Double(12276));
-//		closingPrice.put(new Long(10l), new Double(9405));
-//		closingPrice.put(new Long(11l), new Double(10813));
-//		closingPrice.put(new Long(12l), new Double(11055));
-//		
-//		TechnicalAnalysisService.simpleMovingAverage("", 3, closingPrice);
-//	}
-	
-	
-	public Map<Long, Double> simpleMovingAverage(String symbol) {
-		
-		int PERIODS_AVERAGE = 30;
-		Map<Long, Double> smaMap = new TreeMap<Long,Double>();
 
-		Map<Long, Double> closingPrice = companyRepository.getClosingPrice(symbol);
+	// public static void main(String[] args) {
+	// double[] closePrice = {8938,
+	// 6892,
+	// 9433,
+	// 10692,
+	// 9713,
+	// 8740,
+	// 14300,
+	// 15092,
+	// 12276,
+	// 9405,
+	// 10813,
+	// 11055
+	// };
+
+	// Map<Long, Double> closingPrice = new HashMap<Long, Double>();
+	//
+	// closingPrice.put(new Long(1l), new Double(8938));
+	// closingPrice.put(new Long(2l), new Double(6892));
+	// closingPrice.put(new Long(3l), new Double(9433));
+	// closingPrice.put(new Long(4l), new Double(10692));
+	// closingPrice.put(new Long(5l), new Double(9713));
+	// closingPrice.put(new Long(6l), new Double(8740));
+	// closingPrice.put(new Long(7l), new Double(14300));
+	// closingPrice.put(new Long(8l), new Double(15092));
+	// closingPrice.put(new Long(9l), new Double(12276));
+	// closingPrice.put(new Long(10l), new Double(9405));
+	// closingPrice.put(new Long(11l), new Double(10813));
+	// closingPrice.put(new Long(12l), new Double(11055));
+	//
+	// TechnicalAnalysisService.simpleMovingAverage("", 3, closingPrice);
+	// }
+
+	public Map<Long, Double> simpleMovingAverage(String symbol) {
+
+		int PERIODS_AVERAGE = 30;
+		Map<Long, Double> smaMap = new TreeMap<Long, Double>();
+
+		Map<Long, Double> closingPrice = companyRepository
+				.getClosingPrice(symbol);
 
 		if (closingPrice.size() < PERIODS_AVERAGE + 1) {
 			throw new InsufficientDataException("Not enought data to calculate");
@@ -153,30 +127,84 @@ public class TechnicalAnalysisService {
 			index++;
 		}
 
+		// c.ad
+
 		int TOTAL_PERIODS = price.length;
 
-		double[] out = new double[TOTAL_PERIODS - PERIODS_AVERAGE +1];
+		double[] out = new double[TOTAL_PERIODS - PERIODS_AVERAGE + 1];
 		MInteger begin = new MInteger();
 		MInteger length = new MInteger();
 
-		Core c = new Core();
 		RetCode retCode = c.sma(0, price.length - 1, price, PERIODS_AVERAGE,
 				begin, length, out);
 
 		if (retCode == RetCode.Success) {
-			for(int i = 0; i< out.length; i++) {
-				if (i == out.length -1) {
-					//get the last date and add one more data
-					DateTime dt = new DateTime(dates[dates.length -1]);
-					dt = dt.plusDays(1); 
+			for (int i = 0; i < out.length; i++) {
+				if (i == out.length - 1) {
+					// get the last date and add one more data
+					DateTime dt = new DateTime(dates[dates.length - 1]);
+					dt = dt.plusDays(1);
 					smaMap.put(dt.toDate().getTime(), out[i]);
-					
+
 				} else {
 					smaMap.put(dates[PERIODS_AVERAGE + i], out[i]);
 				}
 			}
 			return smaMap;
-//			maMap
+			// maMap
+		} else {
+			System.out.println("Error");
+			return null;
+		}
+	}
+
+	public Map<Long, Double> exponentialMovingAverage(String symbol) {
+
+		int PERIODS_AVERAGE = 30;
+		Map<Long, Double> emaMap = new TreeMap<Long, Double>();
+
+		Map<Long, Double> closingPrice = companyRepository
+				.getClosingPrice(symbol);
+
+		if (closingPrice.size() < PERIODS_AVERAGE + 1) {
+			throw new InsufficientDataException("Not enought data to calculate");
+		}
+
+		long[] dates = new long[closingPrice.size()];
+		double[] price = new double[closingPrice.size()];
+
+		int index = 0;
+		for (Long key : closingPrice.keySet()) {
+			dates[index] = key;
+			price[index] = closingPrice.get(key);
+			index++;
+		}
+
+		// c.ad
+
+		int TOTAL_PERIODS = price.length;
+
+		double[] out = new double[TOTAL_PERIODS - PERIODS_AVERAGE + 1];
+		MInteger begin = new MInteger();
+		MInteger length = new MInteger();
+
+		RetCode retCode = c.ema(0, price.length - 1, price, PERIODS_AVERAGE,
+				begin, length, out);
+
+		if (retCode == RetCode.Success) {
+			for (int i = 0; i < out.length; i++) {
+				if (i == out.length - 1) {
+					// get the last date and add one more data
+					DateTime dt = new DateTime(dates[dates.length - 1]);
+					dt = dt.plusDays(1);
+					emaMap.put(dt.toDate().getTime(), out[i]);
+
+				} else {
+					emaMap.put(dates[PERIODS_AVERAGE + i], out[i]);
+				}
+			}
+			return emaMap;
+			// maMap
 		} else {
 			System.out.println("Error");
 			return null;
